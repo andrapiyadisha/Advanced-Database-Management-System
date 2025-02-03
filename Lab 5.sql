0d3bfdd0b2539a682d0a3af06cafed345d73fd7f
Disha Andrapiya <dishaandrapiya1812@gmail.com>
2025-02-03 22:47:44 +0530

    Trigger

---
+++ Lab 5.sql
@@ -0,0 +1,201 @@
+-- Creating PersonInfo Table
+CREATE TABLE PersonInfo (
+ PersonID INT PRIMARY KEY,
+ PersonName VARCHAR(100) NOT NULL,
+ Salary DECIMAL(8,2) NOT NULL,
+ JoiningDate DATETIME NULL,
+ City VARCHAR(100) NOT NULL,
+ Age INT NULL,
+ BirthDate DATETIME NOT NULL
+);
+-- Creating PersonLog Table
+CREATE TABLE PersonLog (
+ PLogID INT PRIMARY KEY IDENTITY(1,1),
+ PersonID INT NOT NULL,
+ PersonName VARCHAR(250) NOT NULL,
+ Operation VARCHAR(50) NOT NULL,
+ UpdateDate DATETIME NOT NULL,
+);
+
+--Part – A
+
+--1. Create a trigger that fires on INSERT, UPDATE and DELETE operation on the PersonInfo table to display
+--a message “Record is Affected.”
+  --Insert
+Create Trigger TR_Insert
+on PersonInfo
+After Insert,update,Delete
+As
+Begin 
+	print 'Record is Affected'
+End;
+--2. Create a trigger that fires on INSERT, UPDATE and DELETE operation on the PersonInfo table. For that,
+--log all operations performed on the person table into PersonLog.
+CREATE TRIGGER tr_Person_after_Insert
+ON PersonInfo
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @PersonID int;
+	DECLARE @PersonName varchar(max)
+
+	select @PersonID = PersonID from inserted
+	select @PersonName = PersonName from inserted
+
+	Insert into PersonLog values (@PersonID, @PersonName, 'INSERT', GETDATE());
+END;
+
+CREATE TRIGGER tr_Person_after_Update
+ON PersonInfo
+AFTER update
+AS
+BEGIN
+	DECLARE @PersonID int;
+	DECLARE @PersonName varchar(max)
+
+	select @PersonID = PersonID from inserted
+	select @PersonName = PersonName from inserted
+
+	Insert into PersonLog values (@PersonID, @PersonName, 'Update', GETDATE());
+END;
+
+CREATE TRIGGER tr_Person_after_Delete
+ON PersonInfo
+AFTER DELETE
+AS
+BEGIN
+	DECLARE @PersonID int;
+	DECLARE @PersonName varchar(max)
+
+	select @PersonID = PersonID from inserted
+	select @PersonName = PersonName from inserted
+
+	Insert into PersonLog values (@PersonID, @PersonName, 'DELETE', GETDATE());
+END;
+
+--3. Create an INSTEAD OF trigger that fires on INSERT, UPDATE and DELETE operation on the PersonInfo
+--table. For that, log all operations performed on the person table into PersonLog.
+CREATE TRIGGER tr_Person_instead_Operation
+ON PersonInfo
+INSTEAD OF INSERT, UPDATE, DELETE
+AS
+BEGIN
+	INSERT INTO PersonLog
+	SELECT PersonID, PersonName, 'INSTEAD OF OPERATION', GETDATE() FROM inserted;
+END;
+--4. Create a trigger that fires on INSERT operation on the PersonInfo table to convert person name into
+--uppercase whenever the record is inserted.
+CREATE TRIGGER tr_Person_after_Insert_Name_Uppercase
+ON PersonInfo
+AFTER INSERT
+AS
+BEGIN
+	UPDATE PersonInfo
+	SET PersonName = UPPER(PersonName)
+	WHERE PersonID IN (SELECT PersonID FROM inserted);
+END;
+--5. Create trigger that prevent duplicate entries of person name on PersonInfo table.
+CREATE TRIGGER tr_Prevent_Duplicate_Name
+ON PersonInfo
+INSTEAD OF INSERT
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM PersonInfo P INNER JOIN inserted I ON P.PersonName = I.PersonName)
+    BEGIN
+        PRINT 'Duplicate PersonName is not allowed';
+    END
+    ELSE
+    BEGIN
+        INSERT INTO PersonInfo (PersonID, PersonName, Salary, JoiningDate, City, Age, BirthDate)
+        SELECT PersonID, PersonName, Salary, JoiningDate, City, Age, BirthDate FROM inserted;
+    END
+END;
+
+--6. Create trigger that prevent Age below 18 years.
+CREATE TRIGGER tr_Prevent_Underage
+ON PersonInfo
+INSTEAD OF INSERT
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM inserted WHERE Age < 18)
+	BEGIN
+		PRINT 'Age must be 18 or older';
+	END
+	ELSE
+	BEGIN
+		INSERT INTO PersonInfo (PersonID, PersonName, Salary, JoiningDate, City, Age, BirthDate)
+		SELECT PersonID, PersonName, Salary, JoiningDate, City, Age, BirthDate FROM inserted;
+	END
+END;
+
+--Part – B
+
+--7. Create a trigger that fires on INSERT operation on person table, which calculates the age and update
+--that age in Person table.
+CREATE TRIGGER tr_Person_after_Insert_Calculates_age_Update
+ON PersonInfo
+AFTER INSERT
+AS
+BEGIN 
+	update PersonInfo
+	set Age = DATEDIFF(YEAR,BIRTHDATE,GETDATE())
+	where PersonID IN (SELECT PersonID FROM INSERTED)
+
+END;
+
+--8. Create a Trigger to Limit Salary Decrease by a 10%.
+CREATE TRIGGER tr_Person_after_Insert_Limit_Decrease
+ON PersonInfo
+AFTER INSERT
+AS
+BEGIN 
+	DECLARE @PersonID int, @newSalary int, @oldSalary int;
+
+	update PersonInfo
+	set Salary = Salary - (Salary*0.1)
+	where PersonID = @PersonID
+	
+	select @newSalary = Salary from inserted
+	select @oldSalary = Salary from deleted
+
+	if @newSalary < @oldSalary*0.9
+	Begin
+		update PersonInfo
+		set Salary = @oldSalary 
+		where PersonID = @PersonID
+	End
+
+END;
+
+--Part – C
+
+--9. Create Trigger to Automatically Update JoiningDate to Current Date on INSERT if JoiningDate is NULL
+--during an INSERT.
+CREATE TRIGGER tr_Person_Date_Diffrence
+ON PersonInfo
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @PersonID int;
+	DECLARE @joiningdate datetime;
+
+	select @PersonID = PersonID from inserted
+	select @joiningdate = JoiningDate from inserted
+
+    if @joiningdate is null
+	begin
+		update PersonInfo
+		set JoiningDate = GETdate()
+		where PersonID = @PersonID 
+	end
+
+END
+
+--10. Create DELETE trigger on PersonLog table, when we delete any record of PersonLog table it prints
+--‘Record deleted successfully from PersonLog’.Create Trigger TR_Delete_PersonLog
+on PersonLog
+After Delete
+As
+Begin 
+	print 'Record deleted successfully from PersonLog'
+End;
---
+++ Trigger Extra Lab.sql
@@ -0,0 +1,271 @@
+--AFTER Trigger
+CREATE TABLE EMPLOYEEDETAILS
+(
+	EmployeeID Int Primary Key,
+	EmployeeName Varchar(100) Not Null,
+	ContactNo Varchar(100) Not Null,
+	Department Varchar(100) Not Null,
+	Salary Decimal(10,2) Not Null,
+	JoiningDate DateTime Null
+)
+
+CREATE TABLE EmployeeLogs (
+    LogID INT PRIMARY KEY IDENTITY(1,1),
+    EmployeeID INT NOT NULL,
+	EmployeeName VARCHAR(100) NOT NULL,
+    ActionPerformed VARCHAR(100) NOT NULL,
+    ActionDate DATETIME NOT NULL
+)
+
+--1)Create a trigger that fires AFTER INSERT, UPDATE, and DELETE operations on the EmployeeDetails table to display the message "Employee record inserted", "Employee record updated", "Employee record deleted"
+CREATE TRIGGER TR_INSERT_EXT
+ON EMPLOYEEDETAILS
+AFTER INSERT
+AS
+BEGIN
+	PRINT 'Employee record inserted'
+END
+
+CREATE TRIGGER TR_UPDATE_EXT
+ON EMPLOYEEDETAILS
+AFTER UPDATE
+AS
+BEGIN
+	PRINT 'Employee record updated'
+END
+
+CREATE TRIGGER TR_DELETE_EXT
+ON EMPLOYEEDETAILS
+AFTER DELETE
+AS
+BEGIN
+	PRINT 'Employee record deleted'
+END
+
+--2)Create a trigger that fires AFTER INSERT, UPDATE, and DELETE operations on the EmployeeDetails table to log all operations into the EmployeeLog table.
+CREATE TRIGGER tr_EmployeeDetails_after_Insert
+ON EMPLOYEEDETAILS
+AFTER INSERT
+AS
+BEGIN
+	Declare @EmpID int;
+	Declare @EmpName varchar(100);
+
+	select @EmpID = EmployeeID from inserted
+	Select @EmpName = EmployeeName from inserted
+
+	Insert into EmployeeLogs
+	values(@EmpID, @EmpName, 'Insert', getdate())
+
+END;
+
+CREATE TRIGGER tr_EmployeeDetails_after_Update
+ON EMPLOYEEDETAILS
+AFTER Update
+AS
+BEGIN
+	Declare @EmpID int;
+	Declare @EmpName varchar(100);
+
+	select @EmpID = EmployeeID from inserted
+	Select @EmpName = EmployeeName from inserted
+
+	Insert into EmployeeLogs
+	values(@EmpID, @EmpName, 'Update', getdate())
+
+END;
+
+CREATE TRIGGER tr_EmployeeDetails_after_Delete
+ON EMPLOYEEDETAILS
+AFTER Delete
+AS
+BEGIN
+	Declare @EmpID int;
+	Declare @EmpName varchar(100);
+
+	select @EmpID = EmployeeID from inserted
+	Select @EmpName = EmployeeName from inserted
+
+	Insert into EmployeeLogs
+	values(@EmpID, @EmpName, 'Delete', getdate())
+
+END;
+
+--3)Create a trigger that fires AFTER INSERT to automatically calculate the joining bonus (10% of the salary) for new employees and update a bonus column in the EmployeeDetails table.
+	CREATE TRIGGER TR_BONUS
+	ON EMPLOYEEDETAILS
+	AFTER INSERT
+	AS
+	BEGIN
+		declare @empId int;
+		select @empId= EmployeeID from inserted
+	
+		Update EmployeeDetails
+		set Salary = Salary + Salary*0.1
+		where EmployeeID = @empId
+
+	END
+
+--4)Create a trigger to ensure that the JoiningDate is automatically set to the current date if it is NULL during an INSERT operation.
+	CREATE TRIGGER TR_DATE
+	ON EMPLOYEEDETAILS
+	AFTER INSERT
+	AS
+	BEGIN
+		declare @empId int;
+		declare @JoiningDate datetime;
+		select @empId= EmployeeID from inserted;
+		select @JoiningDate = JoiningDate from inserted
+
+		if @JoiningDate IS Null
+			BEGIN
+				Update EMPLOYEEDETAILS
+				set JoiningDate = getdate()
+				where EmployeeID = @empId 
+			END
+
+	END
+
+--5)Create a trigger that ensure that ContactNo is valid during insert and update (Like ContactNo length is 10)
+	CREATE TRIGGER TR_VALIDCONTACT
+	ON EMPLOYEEDETAILS
+	AFTER INSERT,UPDATE
+	AS
+	BEGIN
+		declare @empId int;
+		declare @Contact datetime;
+		select @empId= EmployeeID from inserted;
+		select @Contact = ContactNo from inserted
+
+		if Len(@Contact)=10
+		BEGIN
+			print'VALID';
+		END
+		ELSE
+		BEGIN
+			PRINT 'NOTE VALID'
+		END
+	END
+
+--Instead of Trigger
+
+CREATE TABLE Movies (
+    MovieID INT PRIMARY KEY,
+    MovieTitle VARCHAR(255) NOT NULL,
+    ReleaseYear INT NOT NULL,
+    Genre VARCHAR(100) NOT NULL,
+    Rating DECIMAL(3, 1) NOT NULL,
+    Duration INT NOT NULL
+);
+
+CREATE TABLE MoviesLog
+(
+	LogID INT PRIMARY KEY IDENTITY(1,1),
+	MovieID INT NOT NULL,
+	MovieTitle VARCHAR(255) NOT NULL,
+	ActionPerformed VARCHAR(100) NOT NULL,
+	ActionDate	DATETIME  NOT NULL
+);
+SELECT *FROM Movies
+
+--1.Create an INSTEAD OF trigger that fires on INSERT, UPDATE and DELETE operation on the Movies table. For that, log all operations performed on the Movies table into MoviesLog.
+	CREATE or alter TRIGGER tr_Movie_insted_Insert
+	ON Movies
+	Instead of INSERT
+	AS
+	BEGIN
+		Declare @movieId int;
+		Declare @title varchar(100);
+
+		select @movieId = movieId from inserted
+		Select @title = movietitle from inserted
+
+		Insert into MoviesLog
+		values(@movieId, @title, 'Insert', getdate())
+
+	END;
+
+	CREATE TRIGGER tr_Movie_instead_update
+	ON Movies
+	Instead of update
+	AS
+	BEGIN
+		Declare @movieId int;
+		Declare @title varchar(100);
+
+		select @movieId = movieId from inserted
+		Select @title = movietitle from inserted
+
+		Insert into MoviesLog
+		values(@movieId, @title, 'Update', getdate())
+
+	END;
+
+	CREATE TRIGGER tr_Movie_instead_Delete
+	ON Movies
+	Instead of Delete
+	AS
+	BEGIN
+		Declare @movieId int;
+		Declare @title varchar(100);
+
+		select @movieId = movieId from inserted
+		Select @title = movietitle from inserted
+
+		Insert into MoviesLog
+		values(@movieId, @title, 'Delete', getdate())
+
+	END;
+--2.Create a trigger that only allows to insert movies for which Rating is greater than 5.5 .
+	CREATE or alter TRIGGER tr_Movie_instead_Insert_Rating
+	ON Movies
+	Instead of INSERT
+	AS
+	BEGIN
+
+		INSERT INTO MoviesLog (MovieID, MovieTitle,  ReleaseYear,Genre, Rating,duration)
+        SELECT MovieID, MovieTitle, ReleaseYear,Genre, Rating,duration
+        FROM inserted
+		where Rating>5.5;
+		
+	END;
+--3.Create trigger that prevent duplicate 'MovieTitle' of Movies table and log details of it in MoviesLog table.
+Create or alter trigger tr_Insteadof_insert_Movie_duplicate
+on Movies
+instead of Insert
+as
+begin
+	INSERT INTO Movies (MovieID, MovieTitle,  ReleaseYear,Genre, Rating,duration)
+        SELECT MovieID, MovieTitle, ReleaseYear,Genre, Rating,duration
+        FROM inserted
+		where MovieTitle Not In (Select MovieTitle from inserted);
+
+end
+
+
+--4.Create trigger that prevents to insert pre-release movies.
+Create or alter trigger tr_Insteadof_insert_Movie_PreRelease
+on Movies
+instead of Insert
+as
+begin
+	INSERT INTO Movies (MovieID, MovieTitle,  ReleaseYear,Genre, Rating,duration)
+        SELECT MovieID, MovieTitle, ReleaseYear,Genre, Rating,duration
+        FROM inserted
+		where Year(ReleaseYear)>Year(GetDate());
+
+end
+
+
+--5.Develop a trigger to ensure that the Duration of a movie cannot be updated to a value greater than 120 minutes (2 hours) to prevent unrealistic entries.
+Create or alter trigger tr_Insteadof_insert_Movie_Duration
+on Movies
+instead of Insert
+as
+begin
+	INSERT INTO Movies (MovieID, MovieTitle,  ReleaseYear,Genre, Rating,duration)
+        SELECT MovieID, MovieTitle, ReleaseYear,Genre, Rating,duration
+        FROM inserted
+		where Duration>120;
+
+end
